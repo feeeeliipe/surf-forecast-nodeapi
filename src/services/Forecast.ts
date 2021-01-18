17292cd472d2172bebdc2b445863fb47e18ea4db
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-18 19:15:44 -0300

    [FEAT]: C03P01 - Forecast Service - Aplicando Clean Archtecture

---
+++ src/services/Forecast.ts
@@ -0,0 +1,43 @@
+import { ForecastPoint, StormGlass } from '../clients/stormGlass';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<BeachForecast[]> {
+    const pointsWithCorretSources: BeachForecast[] = [];
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map((e) => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1,
+        },
+        ...e,
+      }));
+      pointsWithCorretSources.push(...enrichedBeachData);
+    }
+    return pointsWithCorretSources;
+  }
+}
