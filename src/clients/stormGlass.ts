9c43b27a4d259c9db64ed703d42a51aac1ae95aa
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-18 16:29:36 -0300

    [FEAT]: Criando client para tratar requests HTTP e mockando testes

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,6 +1,6 @@
-import { AxiosStatic } from 'axios';
 import { InternalError } from '../../src/util/errors/internal-error';
 import config, { IConfig } from 'config';
+import * as HTTPUtil from '../util/request';
 
 export interface StormGlassPointSource {
   [key: string]: number;
@@ -57,7 +57,7 @@ export class StormGlass {
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
 
-  constructor(protected request: AxiosStatic) {}
+  constructor(protected request = new HTTPUtil.Request()) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     try {
@@ -75,7 +75,7 @@ export class StormGlass {
       );
       return this.normalizeResponse(response.data);
     } catch (error) {
-      if (error.response && error.response.status) {
+      if (HTTPUtil.Request.isRequestError(error)) {
         throw new StormGlassResponseError(
           `Error: ${JSON.stringify(error.response.data)} Code: ${
             error.response.status
