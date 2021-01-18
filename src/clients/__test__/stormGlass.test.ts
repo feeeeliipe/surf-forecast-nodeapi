d8ba8f2dffe965e994a6b71c2e75580777d7e45f
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-17 22:22:52 -0300

    [FEAT]: C02P01 - Criando o client do StormGlass, normalizando a resposta e utilizando mocks com Jest e TS

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,21 @@
+import { StormGlass } from '../../../src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '../../../test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '../../../test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -26.9156352;
+    const lng = -48.676864;
+
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
