314497f5a5299658dc51e86ffe399022cccd0ad5
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-18 22:53:52 -0300

    [FEAT]: C04P02 - Criando testes funcionais para o model de praia (Beaches)

---
+++ test/functional/beaches.test.ts
@@ -0,0 +1,16 @@
+describe('Beaches functional tests', () => {
+  describe('When creating a beach', () => {
+    it('should create a beach with success', async () => {
+      const newBeach = {
+        lat: -26.940075,
+        lng: -48.6396993,
+        name: 'Praia Brava - Itajaí',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+  });
+});
