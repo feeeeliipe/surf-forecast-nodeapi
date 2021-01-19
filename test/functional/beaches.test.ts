c9eb4549b2ed0c5deacc4048258839a609677881
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-18 23:20:50 -0300

    [FEAT]: C04P03 - Adicionando o Beach Model com Mongoose

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,10 @@
+import { Beach } from '../../src/models/beach';
+
 describe('Beaches functional tests', () => {
+  beforeAll(async () => {
+    await Beach.deleteMany({});
+  });
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -11,6 +17,26 @@ describe('Beaches functional tests', () => {
       const response = await global.testRequest.post('/beaches').send(newBeach);
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should throw 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
+    });
+
+    it.skip('should return 500 when there any error other than validation error', async () => {
+      //TODO think in a way to throw a 500
     });
   });
 });
